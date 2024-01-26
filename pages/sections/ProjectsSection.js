2dcb455c209b5085c42dc25b609861a6ca64d0ff
wreckingralph06 <ralphwaldocandaza@gmail.com>
2024-01-26 18:01:41 +0800

    Added a link to a deployed project in the Projects Section

--- pages/sections/ProjectsSection.js
+++ pages/sections/ProjectsSection.js
@@ -1,9 +1,13 @@
 import React from "react";
-import ch_dashboard from "/public/chazydes/ch_dashboard.png";
-import ch_item_list from "/public/chazydes/ch_item_list.png";
-import ch_item_details_page from "/public/chazydes/ch_item_details_page.png";
-import ch_transactions_list from "/public/chazydes/ch_transactions_list.png";
+// import ch_dashboard from "/public/chazydes/ch_dashboard.png";
+// import ch_item_list from "/public/chazydes/ch_item_list.png";
+// import ch_item_details_page from "/public/chazydes/ch_item_details_page.png";
+// import ch_transactions_list from "/public/chazydes/ch_transactions_list.png";
+import gallery from "/public/zeta-retros/gallery.png";
+import item_details_page from "/public/zeta-retros/item_details_page.png";
+import shopping_cart_page from "/public/zeta-retros/shopping_cart_page.png";
 import ImageContainer from "/components/ImageContainer";
+import Link from "next/link";
 // import ProjectsCarousel from "/components/ProjectsCarousel";
 
 function ProjectsSection() {
@@ -15,7 +19,38 @@ function ProjectsSection() {
       <h3 className="text-3xl py-1 dark:text-white ">Projects</h3>
       <hr className="my-4 h-px bg-gray-200 border-2 dark:bg-gray-700"></hr>
       {/* <ProjectsCarousel /> */}
-      <h2 className="text-2xl py-1 dark:text-white ">Chazydes</h2>
+      <Link
+        href="https://zeta-retros-server-az9j64inu-wreckingralph06.vercel.app/products"
+        className="project-1"
+      >
+        <h2 className="text-2xl py-1 dark:text-white ">Zeta-Retros</h2>
+        <div className="all-projects-container">
+          <div className="project-container">
+              <ImageContainer
+                imgClass={"gallery"}
+                imageKey={gallery}
+              />
+            <h5 className="project-header">Gallery</h5>
+          </div>
+          <div className="project-container">
+              <ImageContainer
+                imgClass={"item_details_page"}
+                imageKey={item_details_page}
+              />
+            <h5 className="project-header">Item Details Page</h5>
+          </div>
+          <div className="project-container">
+              <ImageContainer
+                imgClass={"shopping_cart_page"}
+                imageKey={shopping_cart_page}
+              />
+            <h5 className="project-header">Shopping Cart</h5>
+          </div>
+        </div>
+      </Link>
+      
+      {/* Second Project that is not deployed yet */}
+      {/* <h2 className="text-2xl py-1 dark:text-white ">Chazydes</h2>
       <div className="all-projects-container">
         <div className="project-container">
           <a>
@@ -53,7 +88,7 @@ function ProjectsSection() {
           </a>
           <h5 className="project-header">Transactions List</h5>
         </div>
-      </div>
+      </div> */}
     </div>
   );
 }
